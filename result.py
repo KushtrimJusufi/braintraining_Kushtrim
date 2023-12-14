33935c59cd6cb6857424de996756d0f9e49c2686
po05abn <po05abn@eduvaud.ch>
2023-12-14 16:54:57 +0100

    j'ai regler les problèmes d'affichage

--- result.py
+++ result.py
@@ -8,28 +8,62 @@ categorie_result = [('Elève', 'Exercice', 'Date heure', 'Temps', 'nb Total', 'n
 # les catégorie des totales
 categorie_total = [('NBLignes', 'Temps total', 'Nb OK', 'Nb total')]
 # requête SQL qui va prendre les données de la base de données
-def data_result():
-    query = ("select name, exercise, date_hour, duration, nbtrials, nbsuccess from result LIMIT 10")
-    cursor = db_connection.cursor()
-    cursor.execute(query,)
-    data = cursor.fetchall()
-    print(data)
-    return data
+def data_result(pseudo):
+    condition = ""
+    i = 0
+
+    if entry_pseudo.get() == "" and entry_exercice.get() == "" and entry_date_start.get() == "" and entry_date_end.get() == "":
+        query = ("select name, exercise, date_hour, duration, nbtrials, nbsuccess from result LIMIT 10")
+        cursor = db_connection.cursor()
+        cursor.execute(query,)
+        data = cursor.fetchall()
+        return data
+    else:
+        if entry_pseudo.get() != "":
+            condition += f" where name = '{entry_pseudo.get()}' "
+            i += 1
+
+        if entry_exercice.get() != "" and i > 0:
+            condition += f" and exercise = '{entry_exercice.get()}' "
+        elif entry_exercice.get() != "" and i == 0:
+            condition += f" where exercise = '{entry_exercice.get()}' "
+            i += 1
+
+        if entry_date_start.get() != "" and i > 0:
+            condition += f" and date_hour LIKE '{entry_date_start.get()}%' "
+        elif entry_date_start.get() != "" and i == 0:
+            condition += f" where date_hour LIKE '{entry_date_start.get()}%' "
+            i += 1
+
+        query = "select name, exercise, date_hour, duration, nbtrials, nbsuccess from result"
+        query = query + condition
+        print(query)
+        cursor = db_connection.cursor()
+        cursor.execute(query, )
+        data = cursor.fetchall()
+        return data
 
 # requête SQL qui va prendre le total des résultats
-def data_total():
-    query = ("select count(id), SEC_TO_TIME(SUM(TIME_TO_SEC(result.duration))), sum(nbtrials) ,sum(nbsuccess) from result")
-    cursor = db_connection.cursor()
-    cursor.execute(query)
-    total = cursor.fetchall()
-    print(total)
-    return total
+def data_total(e):
+    if entry_pseudo.get() == "":
+        query = ("select count(id), SEC_TO_TIME(SUM(TIME_TO_SEC(result.duration))), sum(nbtrials) ,sum(nbsuccess) from result")
+        cursor = db_connection.cursor()
+        cursor.execute(query)
+        total = cursor.fetchall()
+        return total
+    else:
+        query = ("select count(id), SEC_TO_TIME(SUM(TIME_TO_SEC(result.duration))), sum(nbtrials) ,sum(nbsuccess) from result where name = (%s)")
+        cursor = db_connection.cursor()
+        cursor.execute(query, (e,))
+        total = cursor.fetchall()
+        return total
+
 
 # fonction qui va creer un tableau avec les données
 def disaplay(mytuple, frame):
     for line in range(0,len(mytuple)):
         for col in range(0, len(mytuple[line])):
-            (tk.Label(frame, text=mytuple[line][col], width=16, font=("Arial", 14)).grid(row=line, column=col, padx=2, pady=2))
+            (tk.Label(frame, text=mytuple[line][col], width=16, font=("Arial", 14)).grid(row=line, column=col, padx=2, pady=4))
 
 # cette fonction crée une fenêtre qui affiche les résultats
 def open_window_result(window):
@@ -37,6 +71,10 @@ def open_window_result(window):
     global frame3_2
     global frame5_1
     global frame5_2
+    global entry_pseudo
+    global entry_exercice
+    global entry_date_end
+    global entry_date_start
 
     window_result = tk.Toplevel(window)
 
@@ -90,17 +128,13 @@ def open_window_result(window):
     frame2_2 = tk.Frame(frame2)
     frame2_2.pack(side=BOTTOM, fill=X)
 
-    # bouton inutilisable pour l'instant
-    but_result = tk.Button(frame2_2, text="Voir résultat")
-    but_result.pack(side=LEFT,)
-
     # frame pour les donnée enregistré
     frame3 = tk.Frame(window_result)
     frame3.pack()
 
     # frame pour les catégories
     frame3_1 = tk.Frame(frame3)
-    frame3_1.pack(side=TOP)
+    frame3_1.pack(side=TOP, pady=10)
 
 
     # frame pour les résultats
@@ -125,17 +159,39 @@ def open_window_result(window):
     frame5_2 = tk.Frame(frame5)
     frame5_2.pack()
 
-    # si on rentre dans sa va trier par pseudo
+    # avoir le pseudo dans l'entry
     pseudo = entry_pseudo.get()
 
+    # fonction qui rafraichie les résultats
+    def change_the_result(e):
+        for widget in frame3_2.winfo_children():
+            widget.grid_forget()
+        pseudo = entry_pseudo.get()
+        pseudo = str(pseudo)
+
+        data_result(pseudo)
+        data = data_result(pseudo)
+        disaplay(data, frame3_2)
+
+        data_total(pseudo)
+        total = data_total(pseudo)
+        disaplay(total, frame5_2)
+        return pseudo
+
+
     # affiche le tableau des résultats
-    data_result()
-    data = data_result()
+    data_result(pseudo)
+    data = data_result(pseudo)
     disaplay(categorie_result, frame3_1)
     disaplay(data, frame3_2)
 
-    # affiche le tableau des totals
-    data_total()
-    total = data_total()
+    # affiche le tableau des totaux
+    data_total(pseudo)
+    total = data_total(pseudo)
     disaplay(categorie_total, frame5_1)
     disaplay(total, frame5_2)
+
+    # bouton qui trie les resultats
+    but_result = tk.Button(frame2_2, text="Voir résultat")
+    but_result.pack(side=LEFT, )
+    but_result.bind("<Button-1>", change_the_result)
